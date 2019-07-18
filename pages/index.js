4aa2e3602e554ef9803651901e0d6a3fbd4f3e5c
Mike Wrather <mike.wrather@gmail.com>
2019-07-18 12:59:03 -0700

    sdaf:

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import Head from 'next/head'
 import Header from "../components/header";
 
 export default () => (
   <main>
+    <Head>
+      <meta property="og:image" content="http://example.com/ogp.jpg" />
+      <meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+      <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
+    </Head>
     <Header />
     <section>
       <Link href="/about">
