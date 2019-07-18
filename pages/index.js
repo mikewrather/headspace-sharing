143231e3e7b1832b1977c2fe170d9b39982460e3
Mike Wrather <mike.wrather@gmail.com>
2019-07-18 13:04:30 -0700

    asdf

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ import Header from "../components/header";
 export default () => (
   <main>
     <Head>
-      <meta property="og:image" content="http://example.com/ogp.jpg" />
-      <meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg" />
+      <meta property="og:image" content="https://images.ctfassets.net/v3n26e09qg2r/11CfrjeTLEc8iYUoC0eM2U/7723fe6ae4e779ad09f857eb338473a7/share.jpg" class="next-head" />
+      <meta property="og:url" content="https://www.headspace.com/" class="next-head"></meta>
+      <meta property="og:title" content="Meditation and Sleep Made Simple - Headspace" class="next-head" />
+      <meta property="og:description" content="Live a healthier, happier, more well-rested life in just a few minutes a day with the Headspace app." class="next-head" />
       <meta property="og:image:type" content="image/jpeg" />
-      <meta property="og:image:width" content="400" />
-      <meta property="og:image:height" content="300" />
+      <meta property="og:image:height" content="600" class="next-head" />
+      <meta property="og:image:width" content="1230" class="next-head"></meta>
       <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
     </Head>
     <Header />
